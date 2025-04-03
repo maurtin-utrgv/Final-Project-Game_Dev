3601680212b0ed4b1fa67c9b8fd26a99b6e0595d
maurtin-utrgv <martin.vega02@utrgv.edu>
2025-04-03 17:05:34 -0500

    walking annimations

---
+++ Assets/Scripts/character_movement.cs
@@ -0,0 +1,48 @@
+  using UnityEngine;
+
+public class character_movement : MonoBehaviour
+{
+    float speedX; 
+    float speedY;
+    public float speed;
+    Rigidbody2D rb;
+    private bool isFacingRight = true;
+
+    private Animator anim;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       speedX = Input.GetAxisRaw("Horizontal") * speed;
+       speedY = Input.GetAxisRaw("Vertical") * speed;
+       rb.linearVelocity = new Vector2(speedX, speedY);
+       if(speedX != 0)
+       {
+            anim.SetBool("isWalking", true);
+       }
+       else
+       {
+            anim.SetBool("isWalking", false);
+       }
+       Flip();
+    }
+
+    private void Flip()
+    {
+        if(isFacingRight && speedX > 0f || !isFacingRight && speedX < 0f)
+        {
+            isFacingRight = !isFacingRight;
+            Vector3 localScale = transform.localScale;
+            localScale.x *= -1f;
+            transform.localScale = localScale; 
+        }
+    }
+}
